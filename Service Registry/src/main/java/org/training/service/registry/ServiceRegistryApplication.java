be9c442123ffd8953cf914e00af1e062c93618c0
kartik1502 <kartikkulkarni1411@gmail.com>
2023-08-08 16:19:15 +0530

    added the service registry service.

--- Service Registry/src/main/java/org/training/service/registry/ServiceRegistryApplication.java
+++ Service Registry/src/main/java/org/training/service/registry/ServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package org.training.service.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
     public static void main(String[] args) {
