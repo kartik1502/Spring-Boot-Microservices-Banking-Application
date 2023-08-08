840ab8d486ac719131a333044fcb8a27e962e8db
kartik1502 <kartikkulkarni1411@gmail.com>
2023-08-08 16:10:10 +0530

    initial files for the service registry.

---
+++ Service Registry/src/main/java/org/training/service/registry/ServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package org.training.service.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryApplication.class, args);
+    }
+
+}
