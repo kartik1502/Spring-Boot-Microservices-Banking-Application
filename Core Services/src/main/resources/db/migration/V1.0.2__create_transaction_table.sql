3f07fefa8330b968006839638957ab1a301da20e
kartik1502 <kartikkulkarni1411@gmail.com>
2023-08-09 21:52:31 +0530

    added the database script files for migration.

---
+++ Core Services/src/main/resources/db/migration/V1.0.2__create_transaction_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `transaction`
+(
+    `id`               bigint(20) NOT NULL AUTO_INCREMENT,
+    `amount`           decimal(19, 2) DEFAULT NULL,
+    `transaction_type` varchar(30) NOT NULL,
+    `reference_number` varchar(50) NOT NULL,
+    `transaction_id`   varchar(50) NOT NULL,
+    `account_id`       bigint(20) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY                `FKk9w2ogq595jbe8r2due7vv3xr` (`account_id`),
+    CONSTRAINT `FKk9w2ogq595jbe8r2due7vv3xr` FOREIGN KEY (`account_id`) REFERENCES `bank_account` (`bank_account_id`)
+);
