9cf2a1043671fe6c9c09d57b3aa6ef3ef925e9f3
kartik1502 <kartikkulkarni1411@gmail.com>
2023-08-12 18:05:45 +0530

    updated the name of the table from Transcation -> Transaction.

--- Core Services/src/main/java/org/training/core/services/model/entities/Transaction.java
+++ Core Services/src/main/java/org/training/core/services/model/entities/Transaction.java
@@ -14,7 +14,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Transcation {
+public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
