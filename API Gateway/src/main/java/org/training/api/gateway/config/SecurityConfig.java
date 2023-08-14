232b7d6bc73c7c7eef17610418bf7cdc314816f7
kartik1502 <kartikkulkarni1411@gmail.com>
2023-08-14 19:56:45 +0530

    added the spring security using the OAuth2 and third party security: keycloak, to secure all our APIs.

--- API Gateway/src/main/java/org/training/api/gateway/config/SecurityConfig.java
+++ API Gateway/src/main/java/org/training/api/gateway/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package org.training.api.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
