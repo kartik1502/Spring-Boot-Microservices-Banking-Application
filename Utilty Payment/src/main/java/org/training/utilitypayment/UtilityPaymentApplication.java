5cc9639c42a9d4db74f7f54e2c90919bc0442046
kartik1502 <kartikkulkarni1411@gmail.com>
2023-08-16 20:50:47 +0530

    added initial files.

---
+++ Utilty Payment/src/main/java/org/training/utilitypayment/UtilityPaymentApplication.java
@@ -0,0 +1,13 @@
+package org.training.utilitypayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilityPaymentApplication.class, args);
+    }
+
+}
---
+++ Utilty Payment/src/test/java/org/training/utilitypayment/UtiltyPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training.utilitypayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtiltyPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
