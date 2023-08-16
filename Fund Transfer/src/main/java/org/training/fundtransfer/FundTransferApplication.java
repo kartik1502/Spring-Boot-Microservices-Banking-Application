18a2bde2f05f1dabd79499f77de4583121d6d261
kartik1502 <kartikkulkarni1411@gmail.com>
2023-08-16 18:56:40 +0530

    added the initial files.

---
+++ Fund Transfer/src/main/java/org/training/fundtransfer/FundTransferApplication.java
@@ -0,0 +1,13 @@
+package org.training.fundtransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundTransferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundTransferApplication.class, args);
+    }
+
+}
