e26343981dba7b5a7b5f0aee432f256f081fd1c5
kartik1502 <kartikkulkarni1411@gmail.com>
2024-01-16 22:43:13 +0530

    refactor: modified the code.
    Add some default test cases for the increased code efficiency.

---
+++ API Gateway/src/test/java/org/training/api/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package org.training.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
---
+++ Service Registry/src/test/java/org/training/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,16 @@
+package org.training.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
